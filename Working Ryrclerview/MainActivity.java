e82e0e65b3c209d70a99c2e57af33f89a1b10110
medicma <fireladychicago@yahoo.com>
2018-12-30 20:35:39 -0500

    Add files via upload

---
+++ Working Ryrclerview/MainActivity.java
@@ -0,0 +1,99 @@
+package com.example.firel.nextrecyclerviewexample;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class MainActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+//this is where the recyclerView adapter site
+        RecyclerViewAdapter adapter;
+        RecyclerView recyclerView;
+        private static List<Model> demoData;
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.fragment_main, container, false);
+        }
+
+        @Override
+        public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+            super.onActivityCreated(savedInstanceState);
+            recyclerView = (RecyclerView) getView().findViewById(R.id.myList);
+            recyclerView.setHasFixedSize(true);
+            LinearLayoutManager llm = new LinearLayoutManager(getActivity());
+            llm.setOrientation(LinearLayoutManager.VERTICAL);
+            recyclerView.setLayoutManager(llm);
+
+            demoData = new ArrayList<Model>();
+
+
+            char c = '1';
+            for (byte i = 0; i < 20; i++) {
+                Model model = new Model();
+                model.name = c++;
+                model.age = (byte) (20 + i);
+                demoData.add(model);
+            }
+
+
+            adapter = new RecyclerViewAdapter(demoData);
+            recyclerView.setAdapter(adapter);
+        }
+    }
+}
---
+++ Working Ryrclerview/Model.java
@@ -0,0 +1,8 @@
+package com.example.firel.nextrecyclerviewexample;
+
+import java.util.Date;
+
+public class Model {
+    char name;
+    byte age;
+}
